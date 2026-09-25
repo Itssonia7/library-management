b974576ea8dcc9fb7134c366304532bd94f0ac14
Itssonia7 <soniananvani7@gmail.com>
2026-09-25 17:27:12 +0530

    feat: add Author model

---
+++ src/main/java/com/example/library_management/model/Author.java
@@ -0,0 +1,17 @@
+package com.example.library_management.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Table(name = "authors")
+@Data
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+}
